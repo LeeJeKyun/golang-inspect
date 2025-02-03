58d04c009cf14377adbe05147cc23609d8183450
LeeJeKyun <leejk@metabuild.co.kr>
2025-02-03 09:53:49 +0900

    문자열 처리 6번(leetcode-5) 로컬에선 잘 풀리는데 제출만 하면 안풀림...

--- algorithm-go/leetcode/string/5/LongestPalindromicSubstring.go
+++ algorithm-go/leetcode/string/5/LongestPalindromicSubstring.go
@@ -5,31 +5,32 @@ import "fmt"
 var left, maxLen int
 
 func longestPalindrome(s string) string {
-
 	length := len(s)
 	if length < 2 {
 		return s
 	}
 
 	for i := 0; i < length-1; i++ {
+		fmt.Println(left, maxLen)
 		extendPalindrome(s, i, i+1)
 		extendPalindrome(s, i, i+2)
 	}
 	return s[left : left+maxLen]
 }
 
-func extendPalindrome(s string, l, r int) {
+func extendPalindrome(s string, l, r int) { //문자열, 좌, 우 시작포인터 전달
 	for l >= 0 && r < len(s) && s[l] == s[r] {
 		l--
 		r++
-	}
-	if maxLen < r-l-1 {
-		left = l + 1
-		maxLen = r - l - 1
+		if maxLen < r-l-1 {
+			left = l + 1
+			maxLen = r - l - 1
+		}
 	}
 }
+
 func main() {
-	s := "dcbabcdd"
+	s := "cbbd"
 	palindrome := longestPalindrome(s)
 	fmt.Println(palindrome)
 }
